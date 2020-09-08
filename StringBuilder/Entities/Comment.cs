8770f0348d170d4c6660ff2203b078038196c7cb
Sandro Arpi <sandro.teste72@gmail.com>
2020-09-08 18:26:12 -0300

    Finalizando o projeto

--- StringBuilder/Entities/Comment.cs
+++ StringBuilder/Entities/Comment.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace StringBuilder.Entities
+namespace StringBuilderEx.Entities
 {
     class Comment
     {
--- StringBuilder/Program.cs
+++ StringBuilder/Program.cs
@@ -1,7 +1,7 @@
-﻿using StringBuilder.Entities;
+﻿using StringBuilderEx.Entities;
 using System;
 
-namespace StringBuilder
+namespace StringBuilderEx
 {
     class Program
     {
@@ -10,6 +10,31 @@ namespace StringBuilder
             Comment c1 = new Comment("Have a nice trip!");
             Comment c2 = new Comment("Wow, thats awesome!");
 
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2020 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12
+                );
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night!");
+            Comment c4 = new Comment("May the Force be with you!");
+            Comment c5 = new Comment("Is We brow");
+
+            Post p2 = new Post(
+                DateTime.Parse("28/07/2020 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                5
+                );
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+            p2.AddComment(c5);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
     }
 }
